8522fd6da65095ce1703c41b71f7008570db6c6b
CrystalYeoh <crystalyeohje@gmail.com>
2020-04-17 21:59:33 +0800

    done with part 1

--- ServerWithoutSecurity.java
+++ ServerWithoutSecurity.java
@@ -1,9 +1,17 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+
 
 public class ServerWithoutSecurity {
 
@@ -62,6 +70,65 @@ public class ServerWithoutSecurity {
 						fromClient.close();
 						toClient.close();
 						connectionSocket.close();
+					}
+				}else if (packetType ==2){
+					int numBytes = fromClient.readInt();
+					byte[] block = new byte[numBytes];
+					fromClient.readFully(block, 0, numBytes);
+					System.out.println("Client says: " + new String(block));
+					
+					PrivateKey privKey = PrivateKeyReader.get("Certs/private_key.der");
+					Cipher rsaCipher = Cipher.getInstance("RSA"); 
+					rsaCipher.init(Cipher.ENCRYPT_MODE, privKey);
+					byte[] encryptedBlock = rsaCipher.doFinal(block);
+					System.out.println("Sending to Client the encrypted nonce. ");
+
+					toClient.writeInt(2);
+					toClient.writeInt(encryptedBlock.length);
+					toClient.write(encryptedBlock);
+
+					String msg = null;
+					while(msg == null){
+						packetType = fromClient.readInt();
+						if (packetType == 2){
+							numBytes = fromClient.readInt();
+							block = new byte[numBytes];
+							fromClient.readFully(block, 0, numBytes);
+							System.out.println("Client says: " + new String(block));
+							// Send the filename
+							String filename = "example-bd710400-8079-11ea-ae9d-89114163ae84.crt";
+							toClient.writeInt(0);
+							toClient.writeInt(filename.getBytes().length);
+							toClient.write(filename.getBytes());
+				
+
+							// Open the file
+							FileInputStream fileInputStream = new FileInputStream("Certs/"+filename);
+							BufferedInputStream bufferedFileInputStream = new BufferedInputStream(fileInputStream);
+
+							byte [] fromFileBuffer = new byte[117];
+									
+							// Send the file
+							for (boolean fileEnded = false; !fileEnded;) {
+								numBytes = bufferedFileInputStream.read(fromFileBuffer);
+								fileEnded = numBytes < 117;
+								toClient.writeInt(1);
+								toClient.writeInt(numBytes);
+								toClient.write(fromFileBuffer);
+								toClient.flush();
+							}
+
+							bufferedFileInputStream.close();
+							fileInputStream.close();
+							System.out.println("Sent the CA-verified certificate");
+							
+							msg = "Sent cert";
+							toClient.writeInt(2);
+							toClient.writeInt(msg.getBytes().length);
+							toClient.write(msg.getBytes());
+
+							break;
+						}
 					}
 				}
 
