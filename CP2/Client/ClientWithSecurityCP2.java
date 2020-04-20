ccb0be7f07ca5e2eeb6565094f60ddab27fdd7bc
CrystalYeoh <crystalyeohje@gmail.com>
2020-04-20 16:30:22 +0800

    Hi

---
+++ CP2/Client/ClientWithSecurityCP2.java
@@ -0,0 +1,222 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.Socket;
+import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+import java.util.Date;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class ClientWithSecurityCP2{
+
+	static PublicKey pubkey = null;
+	public static void handshake(DataOutputStream toServer,DataInputStream fromServer, X509Certificate CAcert) throws Exception{
+		
+		String msg_verify = null;
+		String msg_verified = null;
+		String msg_init = null;
+		byte [] msg_verify_bytes =null;
+		FileOutputStream fileOutputStream = null;
+		BufferedOutputStream bufferedFileOutputStream = null;
+		int numBytes = 0;
+
+		// initialising the handshake	
+
+		//Generating NONCE	
+		String dateTimeString = Long.toString(new Date().getTime());
+        byte[] nonceByte = dateTimeString.getBytes();
+		String nonce =  new String(nonceByte);
+		toServer.writeInt(2);
+		toServer.writeInt(nonce.getBytes().length);
+		toServer.write(nonce.getBytes());
+		
+	
+		// waiting for SecStore response
+		while(msg_verify == null){
+			int packetType = fromServer.readInt();
+			if (packetType == 2){
+				System.out.println("receiving msg_verify...");
+				numBytes = fromServer.readInt();
+				msg_verify_bytes = new byte[numBytes];
+				fromServer.readFully(msg_verify_bytes, 0, numBytes);
+				msg_verify = new String(msg_verify_bytes);
+				System.out.println("Received Encrypted block from Server");
+			}
+		}
+		
+		// ask for CA cert to get server's public key
+		msg_init = "Give me your certificate signed by CA";
+		System.out.println("Sending msg_init :" + msg_init);
+		toServer.writeInt(2);
+		toServer.writeInt(msg_init.getBytes().length);
+		toServer.write(msg_init.getBytes());
+
+		// wait for SecStore to provide CA
+		boolean condition = true;
+		while(condition){
+			int packetType = fromServer.readInt();
+			if(packetType==0){
+				numBytes = fromServer.readInt();
+				byte [] filename = new byte[numBytes];
+				fromServer.readFully(filename, 0, numBytes);
+				fileOutputStream = new FileOutputStream("recv_"+new String(filename, 0, numBytes));
+				bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
+
+			}else if (packetType == 1) {
+				numBytes = fromServer.readInt();
+				byte [] block = new byte[numBytes];
+				fromServer.readFully(block, 0, numBytes);
+					if (numBytes > 0){
+						bufferedFileOutputStream.write(block, 0, numBytes);
+					}
+					if (numBytes < 117) {
+
+						if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
+						if (bufferedFileOutputStream != null) fileOutputStream.close();
+						condition = false;
+					}
+
+			}
+		}
+
+		// get public key from certificate
+		//does the name need to be dynamic?
+		InputStream fileInputStream = new FileInputStream("recv_example-bd710400-8079-11ea-ae9d-89114163ae84.crt");
+		CertificateFactory certf = CertificateFactory.getInstance("X.509");
+		X509Certificate serverCert =(X509Certificate)(certf.generateCertificate(fileInputStream));
+
+		//verify that this serverCert is indeed from SecServer
+		serverCert.verify(CAcert.getPublicKey());
+
+		//extract SecServer's public key
+		pubkey = serverCert.getPublicKey();
+		
+		Cipher rsaCipher = Cipher.getInstance("RSA"); 
+		rsaCipher.init(Cipher.DECRYPT_MODE, pubkey);
+		byte[] decryptedBlock = rsaCipher.doFinal(msg_verify_bytes);
+		msg_verified = new String(decryptedBlock);
+		System.out.println("This is decrypted message: " + msg_verified);
+		toServer.flush();
+		if(nonce.compareTo(msg_verified) == 0){
+			System.out.println("decrypted message is the exact same as the nonce. Check succeeded.");
+		}else{System.out.println("decrypted message is not the same as the nonce. Check failed.");}
+
+	}
+	public static void main(String[] args) {
+
+    	String filename = "Homework Set 6.pdf";
+    	if (args.length > 0) filename = args[0];
+
+    	String serverAddress = "localhost";
+    	if (args.length > 1) filename = args[1];
+
+    	int port = 4321;
+    	if (args.length > 2) port = Integer.parseInt(args[2]);
+
+		int numBytes = 0;
+		Socket clientSocket = null;
+
+        DataOutputStream toServer = null;
+        DataInputStream fromServer = null;
+
+    	FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedFileInputStream = null;
+
+		long timeStarted = System.nanoTime();
+
+		try {
+
+			System.out.println("Establishing connection to server...");
+			
+			//Create X509Certificate object
+			InputStream fis = new FileInputStream("cacse.crt");
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			X509Certificate CAcert =(X509Certificate)cf.generateCertificate(fis);
+
+			// //Extract public key from X509Certificate object
+			// PublicKey key = CAcert.getPublicKey();
+
+			//Verify signed certificate
+			//CAcert.verify(key);
+			
+			// Connect to server and get the input and output streams
+			clientSocket = new Socket(serverAddress, port);
+			toServer = new DataOutputStream(clientSocket.getOutputStream());
+			fromServer = new DataInputStream(clientSocket.getInputStream());
+			
+			//Handshake Protocol
+			handshake(toServer, fromServer, CAcert);
+
+			// setting up AES symmetric key
+			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+			keyGen.init(128);
+			SecretKey AESkey = keyGen.generateKey();
+			//Encrypt encodedKey
+			Cipher rsaCipher = Cipher.getInstance("RSA");
+			rsaCipher.init(Cipher.ENCRYPT_MODE, pubkey);
+			byte[] encryptedBlock = rsaCipher.doFinal(AESkey.getEncoded());
+			//Sending AES key
+			System.out.println("Sending AES key...");
+			toServer.writeInt(3);
+			toServer.writeInt(encryptedBlock.length);
+			toServer.write(encryptedBlock);
+			System.out.println("Sent AES key");
+			System.out.println("this is AESkey: " + new String(AESkey.getEncoded()));
+			// Connect to server
+			System.out.println("Sending file...");
+
+
+			// Encrypt file with symmetric key
+			Cipher AESCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			AESCipher.init(Cipher.ENCRYPT_MODE, AESkey);
+			encryptedBlock = AESCipher.doFinal(filename.getBytes());
+			//send encryptedBlock to SecServer
+			toServer.writeInt(0);
+			toServer.writeInt(encryptedBlock.length);
+			toServer.write(encryptedBlock);
+			//toServer.flush();
+
+			// Open the file
+			fileInputStream = new FileInputStream(filename);
+			bufferedFileInputStream = new BufferedInputStream(fileInputStream);
+
+	        byte [] fromFileBuffer = new byte[128];
+			// Send the file
+			int counter = 0;
+	        for (boolean fileEnded = false; !fileEnded;) {
+				//encrypt the file in blocks of 128 bytes
+				numBytes = bufferedFileInputStream.read(fromFileBuffer);
+				encryptedBlock = AESCipher.doFinal(fromFileBuffer);
+				fileEnded = numBytes < 128;
+				Thread.sleep(1);
+				toServer.writeInt(1);
+				System.out.println("this numBytes: " + numBytes);
+				System.out.println("this encryptedBlock.length: " + encryptedBlock.length);
+
+				toServer.writeInt(numBytes);
+				toServer.write(encryptedBlock);
+				counter++;
+			 	//toServer.flush();
+	
+			}
+			Thread.sleep(100);
+	        bufferedFileInputStream.close();
+	        fileInputStream.close();
+
+			System.out.println("Closing connection... counter is: " + counter);
+
+
+		} catch (Exception e) {e.printStackTrace();}
+
+		long timeTaken = System.nanoTime() - timeStarted;
+		System.out.println("Program took: " + timeTaken/1000000.0 + "ms to run");
+	}
+}
---
+++ CP2/Server/ServerWithSecurityCP2.java
@@ -0,0 +1,205 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.*;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class ServerWithSecurityCP2 {
+
+	public static PrivateKey getPrivKey(String filename) throws Exception { 
+	  byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+	  PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+	  KeyFactory kf = KeyFactory.getInstance("RSA");
+	  return kf.generatePrivate(spec);
+	}
+
+	public static PublicKey getPubKey(String filename) throws Exception {
+		byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+		X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		return kf.generatePublic(spec);
+  	}
+
+
+
+	public static void main(String[] args) {
+		int count = 0;
+
+    	int port = 4321;
+    	if (args.length > 0) port = Integer.parseInt(args[0]);
+
+		ServerSocket welcomeSocket = null;
+		Socket connectionSocket = null;
+		DataOutputStream toClient = null;
+		DataInputStream fromClient = null;
+
+		FileOutputStream fileOutputStream = null;
+		BufferedOutputStream bufferedFileOutputStream = null;
+
+		SecretKey AESKey= null;
+
+		try {
+			welcomeSocket = new ServerSocket(port);
+			connectionSocket = welcomeSocket.accept();
+			fromClient = new DataInputStream(connectionSocket.getInputStream());
+			toClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+			while (!connectionSocket.isClosed()) {
+
+
+				int packetType = fromClient.readInt();
+				PrivateKey privKey = getPrivKey("private_key.der");
+
+				// If the packet is for transferring the filename
+				if (packetType == 0) {
+
+					System.out.println("Receiving file...");
+
+					int numBytes = fromClient.readInt();
+					byte [] filename = new byte[numBytes];
+					// Must use read fully!
+					// See: https://stackoverflow.com/questions/25897627/datainputstream-read-vs-datainputstream-readfully
+					fromClient.readFully(filename, 0, numBytes);
+					Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding"); 
+					aesCipher.init(Cipher.DECRYPT_MODE,AESKey);
+					byte[] decryptedBlock = aesCipher.doFinal(filename);
+					
+
+					fileOutputStream = new FileOutputStream("recv_"+new String(decryptedBlock, 0, decryptedBlock.length));
+					bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
+
+				// If the packet is for transferring a chunk of the file
+				} else if (packetType == 1) {
+
+					int numBytes = fromClient.readInt();
+					byte [] block = new byte[144];
+					fromClient.readFully(block, 0, 144);
+					// System.out.println("Block received: " +new String(block));
+					System.out.println("Block size: "+ block.length);
+
+					Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding"); 
+					aesCipher.init(Cipher.DECRYPT_MODE,AESKey);
+					byte[] decryptedBlock = null;
+
+					count++;
+					System.out.println(count);
+
+					if (numBytes > 0)
+						decryptedBlock = aesCipher.doFinal(block);
+						bufferedFileOutputStream.write(decryptedBlock, 0, numBytes);
+
+					if (numBytes < 128) {
+						System.out.println("Closing connection...");
+
+						if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
+						if (bufferedFileOutputStream != null) fileOutputStream.close();
+						fromClient.close();
+						toClient.close();
+						connectionSocket.close();
+					}
+				}else if (packetType ==2){
+					int numBytes = fromClient.readInt();
+					byte[] block = new byte[numBytes];
+					fromClient.readFully(block, 0, numBytes);
+					System.out.println("Client says: " + new String(block));
+					
+					
+					Cipher rsaCipher = Cipher.getInstance("RSA"); 
+					rsaCipher.init(Cipher.ENCRYPT_MODE, privKey);
+					byte[] encryptedBlock = rsaCipher.doFinal(block);
+					System.out.println("Sending to Client the encrypted nonce. ");
+
+					toClient.writeInt(2);
+					toClient.writeInt(encryptedBlock.length);
+					toClient.write(encryptedBlock);
+
+					String msg = null;
+					while(msg == null){
+						packetType = fromClient.readInt();
+						if (packetType == 2){
+							numBytes = fromClient.readInt();
+							block = new byte[numBytes];
+							fromClient.readFully(block, 0, numBytes);
+							System.out.println("Client says: " + new String(block));
+							// Send the filename
+							String filename = "example-bd710400-8079-11ea-ae9d-89114163ae84.crt";
+							toClient.writeInt(0);
+							toClient.writeInt(filename.getBytes().length);
+							toClient.write(filename.getBytes());
+				
+
+							// Open the file
+							FileInputStream fileInputStream = new FileInputStream(filename);
+							BufferedInputStream bufferedFileInputStream = new BufferedInputStream(fileInputStream);
+
+							byte [] fromFileBuffer = new byte[117];
+									
+							// Send the file
+							for (boolean fileEnded = false; !fileEnded;) {
+								numBytes = bufferedFileInputStream.read(fromFileBuffer);
+								fileEnded = numBytes < 117;
+								toClient.writeInt(1);
+								toClient.writeInt(numBytes);
+								toClient.write(fromFileBuffer);
+								toClient.flush();
+							}
+
+							bufferedFileInputStream.close();
+							fileInputStream.close();
+							System.out.println("Sent the CA-verified certificate");
+							
+							msg = "Sent cert";
+							toClient.writeInt(2);
+							toClient.writeInt(msg.getBytes().length);
+							toClient.write(msg.getBytes());
+
+							break;
+						}
+					}
+					
+
+
+				}else if (packetType ==3){
+					System.out.println("Receiving AES Key");
+					int numBytes = fromClient.readInt();
+					byte[] block = new byte[numBytes];
+					fromClient.readFully(block, 0, numBytes);
+					//Decrypt the key
+					Cipher rsaCipher = Cipher.getInstance("RSA"); 
+					rsaCipher.init(Cipher.DECRYPT_MODE, privKey);
+					byte[] decodedBlock = rsaCipher.doFinal(block);
+					// decode the base64 encoded string
+					//String decodedString = Base64.getEncoder().encodeToString(decodedBlock);					
+					AESKey = new SecretKeySpec(decodedBlock,0,decodedBlock.length, "AES");					
+					// rebuild key using SecretKeySpec
+					
+					
+					
+				}
+
+			}
+		} catch (Exception e) {e.printStackTrace();}
+
+	}
+
+}
+
+
+ 
+
+
